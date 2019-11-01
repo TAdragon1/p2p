c708ace21c0b1b2140df0f35fa48d9eb67ad5c8c
Tyler Anderson <twa16@case.edu>
2019-11-01 14:30:30 -0400

    Writer class should be done

--- WriterThread.java
+++ WriterThread.java
@@ -1,16 +1,18 @@
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 
 public class WriterThread implements Runnable {
 
     private Socket connectionSocket;
     private PriorityQueue<Message> outgoingMessages;
+    private HashSet<String> sentLog;
 
     public WriterThread(Socket connectionSocket, PriorityQueue<Message> outgoingMessages){
         this.connectionSocket = connectionSocket;
         this.outgoingMessages = outgoingMessages;
-
+        this.sentLog = new HashSet<>();
     }
 
     @Override
@@ -19,13 +21,17 @@ public class WriterThread implements Runnable {
             while (true) {
                 DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-                if (outgoingMessages.size() == 0){
+                if (outgoingMessages.isEmpty()){
                     outgoingMessages.wait();
                 }
 
                 Message message = outgoingMessages.remove();
+                String messageData = message.getMessage();
 
-                outToClient.writeBytes(message.getMessage() + "\n");
+                if (!sentLog.contains(messageData)) {
+                    outToClient.writeBytes(message.getMessage() + "\n");
+                    sentLog.add(messageData);
+                }
             }
         }
         catch (Exception e){
