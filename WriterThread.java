47975c80b444839a1c837dde4eec7cd0f2e3d9c1
Tyler <twa16@case.edu>
2019-11-02 21:22:56 -0400

    fix too many queries

--- WriterThread.java
+++ WriterThread.java
@@ -38,38 +38,35 @@ public class WriterThread implements Runnable {
 
 
                     message = outgoingMessages.remove();
-                }
-                String messageData = message.getMessage();
 
-                if (!sentLog.contains(messageData)) {
-                    if (messageData.charAt(FIRST_INDEX) == Q) {
-                        InetAddress remoteAddress = connectionSocket.getInetAddress();
-                        String remoteIP = remoteAddress.getHostAddress();
-                        String messageContent = messageData.substring(THIRD_INDEX);
-                        String messageIP = messageContent.split("-")[0];
-                        if (remoteIP.equals(messageIP)){
-                            // Then receiver of this query is query originator, don't send
-                        }
-                        else{
+                    String messageData = message.getMessage();
+
+                    if (!sentLog.contains(messageData)) {
+                        if (messageData.charAt(FIRST_INDEX) == Q) {
+                            InetAddress remoteAddress = connectionSocket.getInetAddress();
+                            String remoteIP = remoteAddress.getHostAddress();
+                            String messageContent = messageData.substring(THIRD_INDEX);
+                            String messageIP = messageContent.split("-")[0];
+                            if (remoteIP.equals(messageIP)) {
+                                // Then receiver of this query is query originator, don't send
+                            } else {
+                                outToClient.writeBytes(messageData + "\n");
+                                Printer.print("Query sent");
+                            }
+                        } else if (messageData.equals(HEART)) {
+                            Printer.print("Sending Heart");
                             outToClient.writeBytes(messageData + "\n");
-                            Printer.print("Query sent");
+                        } else if (messageData.equals(BEAT)) {
+                            //Printer.print("Sending beat");
+                            outToClient.writeBytes(messageData + "\n");
+                        } else {
+                            //Printer.print("Sending " + messageData);
+                            outToClient.writeBytes(messageData + "\n");
                         }
-                    }
-                    else if (messageData.equals(HEART)){
-                        Printer.print("Sending Heart");
-                        outToClient.writeBytes(messageData + "\n");
-                    }
-                    else if (messageData.equals(BEAT)){
-                        //Printer.print("Sending beat");
-                        outToClient.writeBytes(messageData + "\n");
-                    }
-                    else{
-                        Printer.print("Sending " + messageData);
-                        outToClient.writeBytes(messageData + "\n");
-                    }
 
-                    if (!messageData.equals(HEART) && !messageData.equals(BEAT)) {
-                        sentLog.add(messageData);
+                        if (!messageData.equals(HEART) && !messageData.equals(BEAT)) {
+                            sentLog.add(messageData);
+                        }
                     }
                 }
             }
